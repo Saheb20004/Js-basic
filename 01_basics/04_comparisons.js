7abafdc90daf06527cb82d5d6b0431c696cfcde7
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2025-02-13 04:17:01 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+//confusing comparison
+console.log(null==0)
+console.log(null>0)
+console.log(null<0)
+console.log(null>=0)
+console.log(null<=0)
+
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+//confusing comparison
+
+//for strict checking we use "==="
+console.log("2"==2)
+
+console.log("2"===2)
