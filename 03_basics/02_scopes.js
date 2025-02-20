9dbaa9bfe1ba22568f94392e6b89cf1cff01f4eb
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2025-02-20 16:19:37 +0000

    Scope level & hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,42 @@ if(true){
 }
 console.log("C=",c);
 console.log("Outside the scope,a =",a);
+
+
+//+++++++++++++++Scope level & mini hoisting++++++++++++++++++++
+function one(){
+    const username="Krish"
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username="Krish"
+    if(username==="Krish"){
+        const website=" youtube"
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+//+++++++++++++++++interesting++++++++++++++++++++++
+console.log(addOne(20));
+function addOne(num){
+    return num+1
+}
+console.log(addOne(20));
+
+// console.log(addTwo(20));          hoisting
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(20))
